b1847e60629d262f009108bee74a709fa2d27e23
MolinaElGringo <diazsebas99@gmail.com>
2025-03-04 10:14:04 +0100

    improved the drawer

--- src/shapes/ShapeDrawer.java
+++ src/shapes/ShapeDrawer.java
@@ -4,17 +4,7 @@ import java.awt.*;
 
 public class ShapeDrawer {
     public static void draw(Shape shape, Graphics g) {
-        switch (shape.getClass().getSimpleName()) {
-            case "Square":
-                ((Square) shape).draw(g);
-                break;
-            case "Circle":
-                ((Circle) shape).draw(g);
-                break;
-            default:
-                shape.draw(g);
-                break;
-        }
+        shape.draw(g);
     }
 
 }
