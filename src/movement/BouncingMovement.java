c67634e60d92916ec8e8fd16b35850f249f610ba
MolinaElGringo <diazsebas99@gmail.com>
2025-03-04 10:52:47 +0100

    added comments

--- src/movement/BouncingMovement.java
+++ src/movement/BouncingMovement.java
@@ -2,34 +2,30 @@ package movement;
 
 import shapes.Shape;
 
+/**
+ * This class implements the MovementStrategy interface and provides the logic for the bouncing movement of a shape.
+ */
 public class BouncingMovement implements MovementStrategy {
     @Override
     public void move(Shape shape, int panelWidth, int panelHeight) {
         shape.setX(shape.getX() + shape.getDx());
         shape.setY(shape.getY() + shape.getDy());
 
-        // Collision avec le bords droite et gauche (si gauche, vitesse négative, si droite, vitesse positive)
         if (shape.getX() < 0) {
-            if(shape.getDx() < 0) {
-                shape.setDx(-shape.getDx());
-            }
+            shape.setX(0);
+            shape.setDx(-shape.getDx());
         } else if (shape.getX() + shape.getSize() > panelWidth) {
-            if(shape.getDx() > 0) {
-                shape.setDx(-shape.getDx());
-            }
+            shape.setX(panelWidth - shape.getSize());
+            shape.setDx(-shape.getDx());
         }
 
-        // Collision avec le bords haut et bas (si haut, vitesse négative, si bas, vitesse positive)
-        if(shape.getY() < 0) {
-            if(shape.getDy() < 0) {
-                shape.setDy(-shape.getDy());
-            }
+        if (shape.getY() < 0) {
+            shape.setY(0);
+            shape.setDy(-shape.getDy());
         } else if (shape.getY() + shape.getSize() > panelHeight) {
-            if(shape.getDy() > 0) {
-                shape.setDy(-shape.getDy());
-            }
+            shape.setY(panelHeight - shape.getSize());
+            shape.setDy(-shape.getDy());
         }
     }
 
-    //à changer car la vitesse de deplacement est la même ainsi que la redirection
 }
--- src/movement/MovementStrategy.java
+++ src/movement/MovementStrategy.java
@@ -2,6 +2,15 @@ package movement;
 
 import shapes.Shape;
 
+/**
+ * This interface provides the method signature for moving a shape strategically.
+ */
 public interface MovementStrategy {
+    /**
+     * This method moves the shape according to the movement strategy.
+     * @param shape The shape to move
+     * @param panelWidth The width of the panel
+     * @param panelHeight The height of the panel
+     */
     void move(Shape shape, int panelWidth, int panelHeight);
 }
