bffb3da4a3b5962a2e1ca44050c78e39b4efb0d7
Cobora2001 <thomasvu@bluewin.ch>
2025-03-18 20:39:41 +0100

    comment bouncingMovement

--- src/movement/BouncingMovement.java
+++ src/movement/BouncingMovement.java
@@ -1,13 +1,24 @@
+// Authors: Thomas Vuilleumier, Sebastian Diaz
+
 package movement;
 
 /**
- * This class implements the MovementStrategy interface and provides the logic for the bouncing movement of a shape.
+ * This class implements the MovementStrategy interface and provides the logic
+ * for the bouncing movement of a movable object.
  */
 public class BouncingMovement implements MovementStrategy {
+
+    // Singleton instance of the BouncingMovement class
     public static BouncingMovement INSTANCE;
 
+    // Field dimensions of the game - must be the same instance as the one used to define the field
+    // in the application
     private FieldDimensions dimensions;
 
+    /**
+     * Returns the singleton instance of the BouncingMovement class.
+     * @return the singleton instance of the BouncingMovement class
+     */
     public static BouncingMovement getInstance() {
         if(INSTANCE == null) {
             return INSTANCE = new BouncingMovement();
@@ -15,20 +26,30 @@ public class BouncingMovement implements MovementStrategy {
         return INSTANCE;
     }
 
+    /**
+     * Sets the field dimensions of the game.
+     * @param dimensions the field dimensions of the game
+     */
     public void setDimensions(FieldDimensions dimensions) {
         this.dimensions = dimensions;
     }
 
-    private BouncingMovement() {
-    }
+    /**
+     * Private constructor for the BouncingMovement class.
+     */
+    private BouncingMovement() {}
 
+    /**
+     * Moves the movable object according to the bouncing movement strategy.
+     * @param model the movable object to move
+     */
     @Override
     public void move(Movable model) {
         model.setX(model.getX() + model.getDx());
         model.setY(model.getY() + model.getDy());
 
         if (model.getX() < dimensions.getMinX()) {
-            model.setX(0);
+            model.setX(dimensions.getMinX());
             model.setDx(-model.getDx());
         } else if (model.getX() + model.getSize() > dimensions.getMaxX()) {
             model.setX(dimensions.getMaxX() - model.getSize());
@@ -36,7 +57,7 @@ public class BouncingMovement implements MovementStrategy {
         }
 
         if (model.getY() < dimensions.getMinY()) {
-            model.setY(0);
+            model.setY(dimensions.getMinY());
             model.setDy(-model.getDy());
         } else if (model.getY() + model.getSize() > dimensions.getMaxY()) {
             model.setY(dimensions.getMaxY() - model.getSize());
